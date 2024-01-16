282ba7fb30568a90190f3fbbee2b78426cf1df22
brandmin <overness@naver.com>
2024-01-16 17:14:33 +0900

    2401116 Upload

--- src/main/java/com/example/firstProject/entity/Article.java
+++ src/main/java/com/example/firstProject/entity/Article.java
@@ -1,9 +1,6 @@
 package com.example.firstProject.entity;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -16,7 +13,7 @@ import lombok.ToString;
 @Getter // 생성자를 추가하지 않아도 자동적으로 추가되는 어노테이션
 public class Article {
     @Id // 엔티티 대표값 지정
-    @GeneratedValue // 자동생성기능 추가
+    @GeneratedValue(strategy = GenerationType.IDENTITY) // 자동생성기능 추가, DB가 id 자동생성
     private Long id;
 
     @Column // title 필드 선언, 테이블의 title열과 연결됨
